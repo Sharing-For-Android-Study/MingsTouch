5544048c4678c1ae68b3c432ac29638f41b030ff
sti1andy <116719002+MyungSeo-Kim@users.noreply.github.com>
2025-05-01 16:22:12 +0900

    feat: 노래 재생 타이머 구현
    - activity_song.xml에 SeekBar, 노래 시간, 좋아요 추가
    - SongActivity.kt에 Timer 클래스 추가 -> 노래 재생 시간 계산 및 UI 업데이트
    - startTimer, setPlayerStatus 함수 추가 -> 타이머 시작 및 재생 상태 업데이트

--- app/src/main/java/com/example/mission3/SongActivity.kt
+++ app/src/main/java/com/example/mission3/SongActivity.kt
@@ -2,6 +2,7 @@ package com.example.mission3
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mission3.databinding.ActivitySongBinding
@@ -10,12 +11,15 @@ class SongActivity : AppCompatActivity() {
     // 뷰 바인딩 객체 선언
     lateinit var binding: ActivitySongBinding // import가 아니라 xml 에 activitySong 없어서
     lateinit var song: Song
+    lateinit var timer : Timer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 바인딩 초기화
         binding = ActivitySongBinding.inflate(layoutInflater)
         // xml에 있는 뷰를 가져와서 설정
         setContentView(binding.root)
+        initSong()
+        setPlayer(song)
 /*
         binding.mainPlayerCl.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java)) // SongActivity로 이동
@@ -47,6 +51,10 @@ class SongActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.interrupt()
+    }
     private fun initSong() {
         if(intent.hasExtra("title")&&intent.hasExtra("singer")){
             song = Song(
@@ -57,28 +65,69 @@ class SongActivity : AppCompatActivity() {
                 intent.getBooleanExtra("isPlaying", false)
             )
         }
+        startTimer()
+    }
+
+    private fun setPlayer(song: Song) {
+        binding.songMusicTitleTv.text = intent.getStringExtra("title")!!
+        binding.songSingerNameTv.text = intent.getStringExtra("singer")!!
+        binding.songStartTimeTv.text = String.format("%02d:%02d", song.second / 60, song.second % 60)
+        binding.songEndTimeTv.text = String.format("%02d:%02d", song.playTime / 60, song.second % 60)
+        binding.songProgressSb.progress = (song.second * 1000 / song.playTime)
     }
     private fun initBottomNavigation() {
         TODO("Not yet implemented")
     }
-/*
-    private fun initSong() {
-        if(intent.hasExtra("title") && intent.hasExtra("singer")){
-            binding.songMusicTitleTv.text = intent.getStringExtra("title")
-            binding.songSingerNameTv.text = intent.getStringExtra("singer")
-        }
-    }
 
- */
-}
 
 private fun SongActivity.setPlayerStatus(isPlaying: kotlin.Boolean) {
-    if(isPlaying) {
+    song.isPlaying = isPlaying
+    timer.isPlaying = isPlaying
+
+    if (isPlaying) {
         binding.songMiniplayerIv.visibility = View.VISIBLE
         binding.songPauseIv.visibility = View.GONE
-    }
-    else {
+    } else {
         binding.songMiniplayerIv.visibility = View.GONE
         binding.songPauseIv.visibility = View.VISIBLE
     }
+}
+    private fun startTimer() {
+        timer = Timer(song.playTime, song.isPlaying)
+        timer.start()
+    }
+    inner class Timer(private val playTime: Int, var isPlaying: Boolean = true):Thread() {
+        private var second : Int = 0
+        private var mills: Float = 0f
+
+        override fun run() {
+            super.run()
+            try {
+                while (true) {
+                    if(second >= playTime){
+                        break
+                    }
+
+                    if(isPlaying) {
+                        sleep(50)
+                        mills += 50
+
+                        runOnUiThread {
+                            binding.songProgressSb.progress = ((mills/playTime)*100).toInt()
+                        }
+                        if(mills%1000== 0f) {
+                            runOnUiThread {
+                                binding.songStartTimeTv.text = String.format("%02d:%02d", second / 60, second % 60)
+                            }
+                            second++
+                        }
+                    }
+                }
+            }catch(e: InterruptedException) {
+                Log.d("song", "스레드가 죽었습니다. ${e.message}")
+            }
+
+        }
+    }
+
 }
